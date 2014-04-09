34f9df0d17acd490dca29e150ecd23a663346add
Jay Hayes <ur@iamvery.com>
2014-04-09 12:04:13 -0500

    Extract helper method for versioned api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  scope module: :v1, constraints: ApiConstraint.new(version: 1) do
+  def api_version(version, &routes)
+    api_constraint = ApiConstraint.new(version: version)
+    scope(module: "v#{version}", constraints: api_constraint, &routes)
+  end
+
+  api_version(1) do
     resources :articles, only: :index
   end
 
-  scope module: :v2, constraints: ApiConstraint.new(version: 2) do
+  api_version(2) do
     resources :articles, only: :index
   end
 end
