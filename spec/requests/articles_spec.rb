169d5346639e906c34e31819fa752da95d32c3a3
Jay Hayes <ur@iamvery.com>
2014-04-09 11:51:40 -0500

    Wrap v1 spec in context

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,14 +2,16 @@ require 'request_spec_helper'
 
 describe 'articles endpoint' do
   describe 'GET #index' do
-    it 'responds with an array of articles' do
-      get '/articles', {}, HTTP_ACCEPT: 'application/json; version=1'
+    describe 'v1' do
+      it 'responds with an array of articles' do
+        get '/articles', {}, HTTP_ACCEPT: 'application/json; version=1'
 
-      json = JSON.parse(response.body)
+        json = JSON.parse(response.body)
 
-      # these values are hard-coded in the controller as example
-      expect(json.count).to eq(1)
-      expect(json.first['name']).to eq('The Things')
+        # these values are hard-coded in the controller as example
+        expect(json.count).to eq(1)
+        expect(json.first['name']).to eq('The Things')
+      end
     end
   end
 end
